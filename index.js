654031b7448f217ca45d93f56e76bf2ba31aea4e
Pabllot07 <pabllot07@outlook.com>
2022-12-14 10:31:14 -0300

    cors

--- index.js
+++ index.js
@@ -10,8 +10,14 @@ import cors from 'cors'
 const app = express();
 
 // MIDDLEWARES
+app.use((req, res, next)=>{
+    res.header("Access-Control-Allow-Credentials", true)
+    next()
+})
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: "http://localhost:3000"
+}))
 app.use(cookieParser())
 
 app.use("/api/auth", authRoutes)
