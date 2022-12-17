888317acb746c7e057a4658b248763da34fc2402
Pabllot07 <pabllot07@outlook.com>
2022-12-17 10:48:56 -0300

    fetching comments

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,5 +1,7 @@
+import { db } from '../connect.js'
+
 export const getComments = (req, res) => {
-        const q = `SELECT c.*, u.id AS userId, name, profilePic FROM comments AS c JOIN users AS u ON (u.id = p.userId) 
+        const q = `SELECT c.*, u.id AS userId, name, profilePic FROM comments AS c JOIN users AS u ON (u.id = c.userId) 
         WHERE c.postId = ? ORDER BY c.createdAt DESC `;
         
         db.query(q, [req.query.postId], (err, data) => {
