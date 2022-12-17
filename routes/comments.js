15b831b8ab8048fb6a9a3fd43ab0bfdd67e78b30
Pabllot07 <pabllot07@outlook.com>
2022-12-17 11:02:12 -0300

    writing a comment

--- routes/comments.js
+++ routes/comments.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { getComments } from '../controllers/comments.js';
+import { getComments, addComment } from '../controllers/comments.js';
 
 const router = express.Router()
 
 router.get("/", getComments)
+router.post("/", addComment)
 
 export default router
