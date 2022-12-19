3f9421c1289793bde8f2822634124e4300ff7f6a
Pabllot07 <pabllot07@outlook.com>
2022-12-19 18:15:25 -0300

    fetching likes

--- routes/likes.js
+++ routes/likes.js
@@ -1,8 +1,8 @@
 import express from 'express';
-import {  } from '../controllers/likes.js';
+import { getLikes } from '../controllers/likes.js';
 
 const router = express.Router()
 
-router.get("", )
+router.get("/", getLikes )
 
 export default router
