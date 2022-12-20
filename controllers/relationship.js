6e38b4db0b8c8f1cb4b209b287b9c8ef892d06b1
Pabllot07 <pabllot07@outlook.com>
2022-12-20 16:57:51 -0300

    fetching posts from profile

--- controllers/relationship.js
+++ controllers/relationship.js
@@ -19,16 +19,16 @@ export const addRelationship = (req,res) => {
     jwt.verify(token, "secretkey", (err, userInfo)=>{
     if(err) return res.status(403).json("Token is not valid!")
     
-    const q ="INSERT INTO likes (`userId`, `postId`) VALUES  (?)";
+    const q ="INSERT INTO relationships (`followerUserId`, `followedUserId`) VALUES  (?)";
     
     const values = [
         userInfo.id,
-        req.body.postId
+        req.body.userId
     ]
 
     db.query(q, [values], (err, data) => {
         if(err) return res.status(500).json(err)
-        return res.status(200).json("Post has been liked!");
+        return res.status(200).json("Following");
     })
     })
 };
@@ -39,12 +39,12 @@ export const deleteRelationship = (req,res) => {
     jwt.verify(token, "secretkey", (err, userInfo)=>{
     if(err) return res.status(403).json("Token is not valid!")
     
-    const q ="DELETE FROM likes WHERE `userId` = ? AND `postId` = ?";
+    const q ="DELETE FROM relationships WHERE `followerUserId` = ? AND `followedUserId` = ?";
 
 
-    db.query(q, [userInfo.id, req.query.postId], (err, data) => {
+    db.query(q, [userInfo.id, req.query.userId], (err, data) => {
         if(err) return res.status(500).json(err)
-        return res.status(200).json("Post has been disliked!");
+        return res.status(200).json("Unfollow");
     })
     })
 };
