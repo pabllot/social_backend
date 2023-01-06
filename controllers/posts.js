46755467b7f57bf128b40f445375394443c8087a
Pabllot07 <pabllot07@outlook.com>
2023-01-06 15:08:27 -0300

    update user

--- controllers/posts.js
+++ controllers/posts.js
@@ -10,7 +10,8 @@ export const getPosts = (req, res) => {
     jwt.verify(token, "secretkey", (err, userInfo)=>{
         if(err) return res.status(403).json("Token is not valid!")
         
-        const q = userId !== "undefined" ? `SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId) WHERE p.userId = ? ` : `SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId) 
+        const q = userId !== "undefined" ? `SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId) WHERE p.userId = ? ORDER BY p.createdAt DESC ` 
+        : `SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId) 
         LEFT JOIN relationships AS r ON (p.userId = r.followedUserId) WHERE r.followerUserId = ? OR p.userId = ?
         ORDER BY p.createdAt DESC `;
         
