e031b7ff6ff2b206c83d0296639a1334c36a899e
Pabllot07 <pabllot07@outlook.com>
2023-01-07 14:31:07 -0300

    password bug corrected

--- controllers/auth.js
+++ controllers/auth.js
@@ -36,7 +36,7 @@ export const login = (req, res) => {
         if(err)return res.status(500).json(err);
         if(data.length === 0) return res.status(404).json("User not found.");
 
-        const checkPassword = bcrypt.compareSync(req.body.password, data[0].password)
+        const checkPassword = bcrypt.compareSync(req.body.password, data[0]?.password)
 
         if(!checkPassword) return res.status(400).json("Wrong password or username")
 
