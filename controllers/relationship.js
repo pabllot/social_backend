fd0b231f00c5614a2d4d66cac8a0ccfaea33e6af
Pabllot07 <pabllot07@outlook.com>
2023-01-07 21:02:42 -0300

    deleteself

--- controllers/relationship.js
+++ controllers/relationship.js
@@ -19,7 +19,7 @@ export const followSelf = (req, res) => {
     jwt.verify(token, "secretkey", (err, userInfo)=>{
     if(err) return res.status(403).json("Token is not valid!")
     
-    const q ="INSERT INTO relationships (`followerUserId`, `followedUserId`) WHERE followedUserId NOT followedUserId VALUES  (?)";
+    const q ="INSERT INTO relationships (`followerUserId`, `followedUserId`)  VALUES  (?)";
     
     const values = [
         userInfo.id,
@@ -64,6 +64,22 @@ export const deleteRelationship = (req,res) => {
 
 
     db.query(q, [userInfo.id, req.query.userId], (err, data) => {
+        if(err) return res.status(500).json(err)
+        return res.status(200).json("Unfollow");
+    })
+    })
+};
+export const deleteSelf = (req,res) => {
+    const token = req.cookies.accessToken;
+    if(!token) return res.status(401).json("Not logged in!");
+
+    jwt.verify(token, "secretkey", (err, userInfo)=>{
+    if(err) return res.status(403).json("Token is not valid!")
+    
+    const q ="DELETE FROM relationships WHERE `followerUserId` = ? AND `followedUserId` = ?";
+
+
+    db.query(q, [userInfo.id, userInfo.id], (err, data) => {
         if(err) return res.status(500).json(err)
         return res.status(200).json("Unfollow");
     })
