20024073a8f3cdce4c8a5032b773db5f97826dc4
Pabllot07 <pabllot07@outlook.com>
2023-01-08 14:22:49 -0300

    deleted followSelf

--- controllers/relationship.js
+++ controllers/relationship.js
@@ -12,27 +12,6 @@ export const getRelationships = (req, res) => {
     
 };
 
-export const followSelf = (req, res) => {
-    const token = req.cookies.accessToken;
-    if(!token) return res.status(401).json("Not logged in!");
-    
-    jwt.verify(token, "secretkey", (err, userInfo)=>{
-    if(err) return res.status(403).json("Token is not valid!")
-    
-    const q ="INSERT INTO relationships (`followerUserId`, `followedUserId`)  VALUES  (?)";
-    
-    const values = [
-        userInfo.id,
-        userInfo.id
-    ]
-    
-    db.query(q, [values], (err, data) => {
-        if(err) return res.status(500).json(err)
-        return res.status(200).json("Following");
-    })
-    })
-}
-
 export const addRelationship = (req,res) => {
     const token = req.cookies.accessToken;
     if(!token) return res.status(401).json("Not logged in!");
@@ -53,6 +32,7 @@ export const addRelationship = (req,res) => {
     })
     })
 };
+
 export const deleteRelationship = (req,res) => {
     const token = req.cookies.accessToken;
     if(!token) return res.status(401).json("Not logged in!");
@@ -64,22 +44,6 @@ export const deleteRelationship = (req,res) => {
 
 
     db.query(q, [userInfo.id, req.query.userId], (err, data) => {
-        if(err) return res.status(500).json(err)
-        return res.status(200).json("Unfollow");
-    })
-    })
-};
-export const deleteSelf = (req,res) => {
-    const token = req.cookies.accessToken;
-    if(!token) return res.status(401).json("Not logged in!");
-
-    jwt.verify(token, "secretkey", (err, userInfo)=>{
-    if(err) return res.status(403).json("Token is not valid!")
-    
-    const q ="DELETE FROM relationships WHERE `followerUserId` = ? AND `followedUserId` = ?";
-
-
-    db.query(q, [userInfo.id, userInfo.id], (err, data) => {
         if(err) return res.status(500).json(err)
         return res.status(200).json("Unfollow");
     })
--- routes/relationships.js
+++ routes/relationships.js
@@ -1,12 +1,10 @@
 import express from 'express';
-import { getRelationships, deleteRelationship, addRelationship, followSelf, deleteSelf } from '../controllers/relationship.js';
+import { getRelationships, deleteRelationship, addRelationship } from '../controllers/relationship.js';
 
 const router = express.Router()
 
 router.get("/", getRelationships)
 router.post("/", addRelationship)
-router.post("/self", followSelf)
-router.delete("/self", deleteSelf)
 router.delete("/", deleteRelationship)
 
 export default router
