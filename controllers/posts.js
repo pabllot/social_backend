dd585bad6d2c504c2fa88c46752750af9730a0cc
Pabllot07 <pabllot07@outlook.com>
2023-01-08 15:35:18 -0300

    you don't follow urself anymore

--- controllers/posts.js
+++ controllers/posts.js
@@ -13,8 +13,8 @@ export const getPosts = (req, res) => {
         const q =
         userId !== "undefined"
           ? `SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId) WHERE p.userId = ? ORDER BY p.createdAt DESC`
-          : `SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId)
-      LEFT JOIN relationships AS r ON (p.userId = r.followedUserId) WHERE r.followerUserId= ? OR p.userId =?
+          : `SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId) 
+      LEFT JOIN relationships AS r ON (p.userId = r.followedUserId) WHERE r.followerUserId= ? 
       ORDER BY p.createdAt DESC`;
         
         const values =
