8649988f2587b93813ec0a39629168922a0f5587
Pabllot07 <pabllot07@outlook.com>
2023-01-14 19:02:36 -0300

    back to square one

--- index.js
+++ index.js
@@ -12,14 +12,13 @@ import multer from 'multer'
 
 dotenv.config()
 
-
 const app = express();
 
 
 // MIDDLEWARES
 app.use((req, res, next)=>{
-  res.header("Access-Control-Allow-Credentials", true)
-  next()
+    res.header("Access-Control-Allow-Credentials", true)
+    next()
 })
 app.use(express.json())
 const whitelist = ['http:/localhost3000', 'https://socialmediapablot.vercel.app/']
@@ -38,25 +37,30 @@ app.use(cors({
 app.use(cookieParser())
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "../client/public/upload");
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + file.originalname);
-  },
-});
-
-const upload = multer({ storage: storage });
-
-app.post("/api/upload", upload.single("file"), (req, res) => {
-  const file = req.file;
-    res.status(200).json(file.filename);
+    destination: function (req, file, cb) {
+      cb(null, "../client/public/upload");
+    },
+    filename: function (req, file, cb) {
+      cb(null, Date.now() + file.originalname);
+    },
   });
   
+  const upload = multer({ storage: storage });
+  
+  app.post("/api/upload", upload.single("file"), (req, res) => {
+    const file = req.file;
+    res.status(200).json(file.filename);
+  });
+
+  app.use("/api/auth", authRoutes)
   app.use("/api/users", userRoutes)
+  app.use("/api/posts", postsRoutes)
+  app.use("/api/comments", commentsRoutes)
+  app.use("/api/likes", likesRoutes)
+  app.use("/api/relationships", relationshipRoutes)
   
   const PORT = process.env.PORT || 8800
-  
+
   app.listen(PORT, ()=>{
     console.log("backend working on port" + PORT)
   })
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,11 @@
 import express from 'express';
-import { getUsers } from '../controllers/user.js';
+import { getUser, updateUser, getUsers, deleteUser } from '../controllers/user.js';
 
 const router = express.Router()
 
 router.get("/", getUsers)
+router.get("/find/:userId", getUser)
+router.put("/", updateUser)
+router.delete("/:id", deleteUser)
 
 export default router
