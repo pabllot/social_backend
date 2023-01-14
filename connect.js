8548833540b2298a490d17fc6636a722c6d961f1
Pabllot07 <pabllot07@outlook.com>
2023-01-13 21:34:38 -0300

    env

--- connect.js
+++ connect.js
@@ -1,8 +1,11 @@
 import mysql from 'mysql'
+import * as dotenv from 'dotenv'
+dotenv.config()
+
 
 export const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "password",
+    password: process.env.PASSWORD_MYSQL,
     database: "social"
 })
