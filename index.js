c48e57d34e9cf46c82374520d257eb8e18d6b527
Pabllot07 <pabllot07@outlook.com>
2023-01-13 21:27:26 -0300

    cors test

--- index.js
+++ index.js
@@ -20,9 +20,7 @@ app.use((req, res, next)=>{
     next()
 })
 app.use(express.json())
-app.use(cors({
-    origin: "socialmediapablot.vercel.app"
-}))
+app.use(cors())
 app.use(cookieParser())
 
 const storage = multer.diskStorage({
