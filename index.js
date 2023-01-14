2722f4c77a3c2167519df9ffaef87923b9a46bef
Pabllot07 <pabllot07@outlook.com>
2023-01-13 21:15:47 -0300

    dominio

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use((req, res, next)=>{
 })
 app.use(express.json())
 app.use(cors({
-    origin: "http://localhost:3000"
+    origin: "https://socialmediapablot.vercel.app"
 }))
 app.use(cookieParser())
 
