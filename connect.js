02318609f67925ed6817fa8627a84455880c178e
Pabllot07 <pabllot07@outlook.com>
2023-01-14 01:13:52 -0300

    create pool db

--- connect.js
+++ connect.js
@@ -2,10 +2,10 @@ import mysql from 'mysql'
 import * as dotenv from 'dotenv'
 dotenv.config()
 
-
-export const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: process.env.PASSWORD_MYSQL,
-    database: "social"
-})
+export const db  = mysql.createPool({
+    connectionLimit : 100,
+    host            : 'localhost',
+    user            : 'root',
+    password        : process.env.PASSWORD_MYSQL,
+    database        : 'social'
+  });
